5f04ba12a5d681682773bbf840c650b084baed05
oladimeji <dimejiola@msn.com>
2022-01-24 22:29:11 +0000

    init commit

---
+++ google_helper.py
@@ -0,0 +1,214 @@
+#@auto-fold regex /./
+from __future__ import print_function
+import pickle, email, os.path,  mimetypes,os
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.http import MediaIoBaseDownload
+import base64,io,python_helper as ph
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email import encoders
+from httplib2 import Http
+from apiclient import errors
+from apiclient.discovery import build
+from apiclient.http import MediaFileUpload
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://mail.google.com/','https://www.googleapis.com/auth/drive','https://www.googleapis.com/auth/calendar']
+
+def main(service):
+    """Use this for the different google api   """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first time.
+    if os.path.exists(r'C:/Users/oolao/github/Projects/secrets/token.pickle'):
+        with open(r'C:/Users/oolao/github/Projects/secrets/token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # creds.refresh(Request())
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                r'C:/Users/oolao/github/Projects/secrets/client_secret.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open(r'C:/Users/oolao/github/Projects/secrets/token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    if service == 'gmail':
+        v = 'v1'
+    else:
+        v ='v3'
+    serviced = build(service, v, credentials=creds)
+    return serviced
+
+def CreateMesssage(to,subject,message_text,cc='',bbc='',attachments=''):
+      """Create Message object"""
+      mimeMessage = MIMEMultipart()
+      mimeMessage['to'] = to
+      mimeMessage['from'] = 'me'
+      mimeMessage['cc'] = cc
+      mimeMessage['bcc'] = bbc
+      mimeMessage['subject'] = subject
+      mimeMessage.attach(MIMEText(message_text, 'html'))
+
+      for attachment in attachments: #list of file_paths
+          content_type, encoding = mimetypes.guess_type(attachment)
+          main_type, sub_type = content_type.split('/', 1)
+          file_name = os.path.basename(attachment)
+          f = open(attachment, 'rb')
+
+          myFile = MIMEBase(main_type, sub_type)
+          myFile.set_payload(f.read())
+          myFile.add_header('Content-Disposition', 'attachment', filename=file_name)
+          email.encoders.encode_base64(myFile)
+          f.close()
+          mimeMessage.attach(myFile)
+
+      return {'raw': base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()}
+
+def SendMessage(message):
+    """Send CreateMessage object"""
+    message = (main('gmail').users().messages().send(userId='me', body=message).execute())
+
+def FindMessage(query):
+    """Return a list of message IDs searched"""
+    resp = main('gmail').users().messages().list(userId='me' ,maxResults = 500, q= '{}'.format(query)).execute()
+    try:
+        ids = [item['id'] for item in resp['messages']]
+        while 'nextPageToken' in resp:
+            resp = main('gmail').users().messages().list(userId='me' ,maxResults = 500, pageToken = resp['nextPageToken']).execute()
+            ids.extend([item['id'] for item in resp['messages']])
+        return ids
+    except:
+        print('No messages found, try again!')
+
+def ReadMessage(id,path=''):
+    """Returns message body and downloads attachment if available"""
+    resp = main('gmail').users().messages().get(userId='me', id= id, format="full").execute()
+    subject = [i['value'] for i in resp.get("payload").get("headers") if i["name"]=="Subject"]
+    files = []
+    body_str= ''
+    for part in resp.get("payload")['parts']:
+        if part.get('mimeType') == 'text/plain':
+            body_str =(base64.urlsafe_b64decode(part.get('body').get('data').encode("ASCII")).decode("utf-8"))
+        if part.get('mimeType') == 'multipart/alternative':
+            for part in part.get('parts'):
+                if part.get('mimeType') == 'text/plain':
+                    body_str =(base64.urlsafe_b64decode(part.get('body').get('data').encode("ASCII")).decode("utf-8"))
+
+        if  'attachmentId' in part['body']:
+            att = main('gmail').users().messages().attachments().get(userId='me',
+                                    messageId=id,id=part['body']['attachmentId']).execute()
+            data = base64.urlsafe_b64decode(att['data'].encode('UTF-8'))
+            files.append(part['filename'])
+            with open(os.path.join(path+part['filename']), 'wb') as f:
+                        f.write(data)
+            body_str = subject[0] +'\n\n' +body_str +'\n\n{} was downloaded into the local directory'.format(part['filename'])
+    print(body_str)
+    return body_str,files
+
+def ModifyLabel(id,label,type):
+    """Add or remove labels from emails"""
+    respd = main('gmail').users().messages().modify(userId='me', id= id, body = {"addLabelIds" if type == 'add' else "removeLabelIds": [label]}).execute()
+    return True
+
+def create_folder(name,sub=None,sub2=None):
+    """Create a new folder if it doesn't exists in google drive"""
+    main('drive').files().emptyTrash().execute()
+    page_token = None
+    fd_name = sub2 if sub2 is not None else sub if sub is not None else  name
+    response =  main('drive').files().list(q="name = '{}' ".format(fd_name.replace("'",r"\'")),
+                                          spaces='drive',
+                                          fields='nextPageToken, files(id, name, mimeType,parents)',
+                                          pageToken=page_token).execute()
+    # TODO: complete this...find the name of the parent folder then search in the parent folder for the sup folder names  'appDataFolder' in parents
+    #https://developers.google.com/drive/api/v3/search-files
+    if len(response['files']) > 0: # if folder already exists
+        print('Folder already exists: %s id: %s' % (response['files'][0]['name'], response['files'][0]['id']))
+        return(response['files'][0]['id'])
+    else:
+        file_metadata = {'name': name,'mimeType': 'application/vnd.google-apps.folder'}
+        file = main('drive').files().create(body=file_metadata, fields='id').execute()
+        if sub:
+            file_metadata = {'name': sub,'mimeType': 'application/vnd.google-apps.folder', 'parents': [file.get('id')]}
+            file = main('drive').files().create(body=file_metadata, fields='id').execute()
+        if sub2:
+            file_metadata = {'name': sub2,'mimeType': 'application/vnd.google-apps.folder', 'parents': [file.get('id')]}
+            file = main('drive').files().create(body=file_metadata, fields='id').execute()
+        return(file.get('id'))
+
+def check_if_folder_exists(folder_name):
+    """Check if a folder exists in the gogole drive"""
+    resp = main('drive').files().list(q="mimeType='application/vnd.google-apps.folder' and name= '{}'".format(folder_name),
+                             spaces='drive',
+                             fields='nextPageToken, files(id, name)').execute()
+    return resp.get('files')
+
+def gdrive_up(f_id,file_name,file_path):
+    """Upload to google drive API using the folder ID"""
+    main('drive').files().emptyTrash().execute()
+    resp =  main('drive').files().list(q="name = '{}' and '{}' in parents ".format(file_name.replace("'",r"\'"),f_id),
+                                          spaces='drive',
+                                          fields='nextPageToken, files(id, name, mimeType,parents)').execute()
+    if len(resp['files']) > 0 : # if the file name doesn't exists the same parent
+        print('File {} already exists in folder {}'.format(file_name, f_id))
+        print(resp['files'])
+    else:
+        folder_id = f_id
+        file_metadata = {
+            'name': name,
+            'parents': [folder_id]}
+        media = MediaFileUpload(fp,
+                                #mimetype='image/png',
+                                resumable=True)
+        file = main('drive').files().create(body=file_metadata,
+                                            media_body=media,
+                                            fields='id').execute()
+
+def find_files(name):
+    """Find files with a specfic name and file type"""
+    page_token = None
+    files = []
+    while True:
+        response = main('drive').files().list(q="name contains '{}' and  trashed = false ".format(name),
+                                              spaces='drive',
+                                              fields='nextPageToken, files(id,name,mimeType)',
+                                              pageToken=page_token).execute()
+        for file in response.get('files', []):
+            # Process change
+            files.append([file.get('name'),'.'+file.get('mimeType')[-3:], file.get('id')])
+        page_token = response.get('nextPageToken', None)
+        if page_token is None:
+            break
+    return files
+
+def download_file(file_name,file_id,location =''):
+    """Download files using the file ID"""
+    request = main('drive').files().get_media(fileId=file_id)
+    fh = io.BytesIO()
+    downloader = MediaIoBaseDownload(fd=fh, request=request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        print("Download %d%%." % int(status.progress() * 100))
+        fh.seek(0)
+        with open(os.path.join(location,file_name),'wb') as f:
+            f.write(fh.read())
+            f.close()
+
+# find_files('google_helper','')
+# root = r'C:\Users\oolao\github\Projects\personal\testing'
+# msg_body ="<html><body>Hi,<br /><br />Your order is available, please see attached.<br /> <br /> With best regards <br /> <b>MGMT </a> </body></html>"
+# Msg = CreateMessage('dimejiola@msn.com',r"for educational purposes only.", msg_body)
+# SendMessage(Msg)
+# ModifyLabel(FindMessage('Unknown Investor')[0],'UNREAD','add')
+# print(ReadMessage(FindMessage('Unknown Investor')[0]))
+# gdrive_up('16N9yJ6x0vSpW-sfdpdsAmSQkcncnLHC8',file_name ='google_helper.py',file_path =r'C:\Users\oolao\github\python-helpers\google_helper.py')
+
+# if __name__ == '__main__':
+#     main()
---
+++ python_helper.py
@@ -0,0 +1,219 @@
+#@auto-fold regex /./
+import json,os,boto3,psycopg2,click,re,shutil
+import gspread_dataframe as gd, gspread_formatting as f, gspread,requests as r
+from oauth2client.service_account import ServiceAccountCredentials
+from datetime import  datetime,timedelta
+
+if os.name == "nt":
+    root_fp =  r"C:/Users/oolao/github/"
+elif os.name == "posix":
+    root_fp = r"/mnt/c/Users/oolao/github/"
+
+creds = json.load(open(root_fp+'creds/creds.json'))
+
+def now(type):
+    """Return the current date/time in different formats"""
+    if type == 1:
+        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    elif type == 2:
+        now = datetime.now().strftime('%H:%M:%S')
+    elif type == 3: ##return the day of week
+        now = datetime.isoweekday(datetime.now())
+    elif type == 4: ##return the day of week
+        now = datetime.today()
+    return now
+
+def date_between(ss,ee):
+    """Return dates between the numbers"""
+    start_date =  (now(4)  - timedelta(days=ss))
+    end_date =  (now(4)  - timedelta(days=ee))
+    return [start_date,end_date]
+
+def s3up(table_name,s3_folder,file_ext='.csv',bucket='bucket_name'):
+    """Upload a file to s3 bucket"""
+    cc = creds.get("aws").get("s3")
+    s3 = boto3.resource('s3',aws_access_key_id=cc.get("access_key_id"), aws_secret_access_key=cc.get("secret_access_key"),region_name =cc.get("region"))
+    s3.meta.client.upload_file(table_name+file_ext, bucket, '%s/%s' %(s3_folder,table_name+file_ext))
+
+def psycopg_con():
+    """Create a connection to redshift"""
+    cc = creds.get("aws").get("redshift")
+    con=psycopg2.connect(dbname=cc.get("db"),host=cc.get("host"),port=cc.get("port"), user=cc.get("user_name"),password=cc.get("password"))
+    return con
+
+def rsup(s3_folder,table_name,file_ext='.csv', bucket="bucket_name"):
+    """Copy data from S3 buckets into tables"""
+    cc = creds.get("aws").get("s3")
+    if file_ext == '.csv':
+        deli  = ','
+    elif file_ext == '.txt' :
+        deli = r'\t'
+    cur = psycopg_con().cursor() # create connection
+    dml = """copy %s.%s from 's3://%s/%s/%s'
+        access_key_id '%s'
+        secret_access_key '%s'
+        region 'us-east-1'
+        ignoreheader 1
+        null as ''
+        removequotes
+        delimiter '%s'
+        escape
+        acceptinvchars ;""" % (s3_folder,table_name,bucket,s3_folder,table_name+file_ext,cc.get("access_key_id"),cc.get("secret_access_key"),deli)
+    cur.execute(dml)
+    cur.connection.commit()
+    cur.connection.close()
+    return cur.statusmessage
+
+def rsexe(sql,type):
+    """Use this to execute queries in redshift.
+    If type = dml a dataframe is return
+    if type = ddl status message is return
+    if type = dl2 data is delete then a temp table is returned
+    """
+    cur = psycopg_con().cursor()
+    if type == 'dml':
+        data=pd.read_sql(sql, cur.connection)
+    elif  type == 'ddl':
+        cur.execute(sql)
+        data = cur.statusmessage
+    elif  type == 'dl2':
+        data=pd.read_sql(sql.split(';')[0]+';'+sql.split(';')[1],cur.connection) # insert data to be deleted into temp table
+        cur.execute(sql.split(';')[2]) # delete data
+    cur.connection.commit()
+    cur.connection.close()
+    return data
+
+def stg_sql(schema, table,date_column, startdate,endate):
+    """         0 = select * into #temp from {0}.{1} where {2}::date>= \'{3}\'and {2}::date <= \'{4}\';
+                1 = select * from #temp;
+                2 = delete from {0}.{1} where {2}::date>= \'{3}\' and {2}::date <= \'{4}\';
+    """
+    return  """ select * into #temp from {0}.{1} where {2}::date>= \'{3}\'and {2}::date <= \'{4}\';
+                select * from #temp;
+                delete from {0}.{1} where {2}::date>= \'{3}\' and {2}::date <= \'{4}\';
+            """.format(schema, table,date_column, startdate,endate)
+
+def s3_redshift(df, s3_folder, table_name,file_ext='.csv'):
+    """ Save file to the working files folder and then move it to S3 bucket then into redshift"""
+    df =  df.convert_dtypes()
+    df.to_csv(table_name+file_ext, sep=',' ,index=False,  encoding='utf-8', line_terminator =None)
+    s3up(table_name,file_ext,s3_folder)
+    rsup(s3_folder,table_name,file_ext)
+    print('Copied data into {0}/{1}/{2} s3 bucket\n '\
+        'Inserted data into {1}.{2}'.format(bucket,s3_folder,table_name))
+
+def mssql(sql,type):
+    """ Connect to SQL Server, Type dml is use to fetch data while ddl is used to modify data"""
+    import pymssql
+    cc = creds.get("sql_sever")
+    conn = pymssql.connect(server=cc.get("sql_sever"), user=cc.get("user_name"), password=cc.get("password"), database=cc.get("db"))
+    cursor = conn.cursor(as_dict=True)
+    cursor.execute(sql)
+    if type == 'dml':
+        data=cursor.fetchall()
+    elif  type == 'ddl':
+        data = None
+    conn.commit()
+    conn.close()
+    return data
+
+def gspread_con():
+    """Create a gspread connection"""
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(re_path()+'creds/svc-holmes.json', scope)
+    gc = gspread.authorize(credentials)
+    return gc
+
+def open_wb(id):
+    """Open workbook"""
+    wb = gspread_con().open_by_key(id)
+    return wb
+
+def num_to_col_letters(num):
+    """numbers to columns"""
+    letters = ''
+    while num:
+        mod = (num - 1) % 26
+        letters += chr(mod + 65)
+        num = (num - 1) // 26
+    return ''.join(reversed(letters))
+
+def rep_data_sh(df,wb_id,sh_name):
+    """Replace data in sheet with dataframe, if sheet doesn't exists then create a new sheet"""
+    top_row_fmt = f.cellFormat(backgroundColor= f.color(1,1,1),#https://tug.org/pracjourn/2007-4/walden/color.pdf
+    textFormat=f.textFormat(bold=False, foregroundColor=f.color(0,0,0)))
+    if sh_name in str(open_wb(wb_id).worksheets()):
+        sheet = open_wb(wb_id).worksheet(sh_name)
+        f.format_cell_range(sheet, 'A1:{}{}'.format(num_to_col_letters(df.shape[1]),df.shape[0]), top_row_fmt) #remove formats
+    else:
+        sheet = open_wb(wb_id).add_worksheet(sh_name, df.shape[0],df.shape[1])
+    f.set_frozen(sheet, rows=0) #Remove top row formats and unfreeze top row
+    sheet.resize(rows=1) #delete all row
+    gd.set_with_dataframe(sheet, df) #insert data
+    sheet.set_basic_filter()
+    new_fmt =  top_row_fmt+ f.cellFormat(backgroundColor=f.color(1,3.5,454))
+    f.format_cell_range(sheet, 'A1:{}1'.format(num_to_col_letters(df.shape[1])), new_fmt)
+
+def ppl_hr(query):
+    """Return contents from the people HR API, requires query keys"""
+    cc = creds.get("ppl_hr").get("api_key")
+    link = 'https://api.peoplehr.net/Query'
+    para = json.dumps({"APIKey": cc.get("pplapikey"),"Action": 'GetQueryResultByQueryName',"QueryName" : query})
+    post = r.post(link,para)
+    content = post.json()['Result']
+    return  content
+
+def get_s3_file_version(bucket,filename):
+    """Downloads list of versions a file"""
+    cc = creds.get("aws").get("s3")
+    s3 = boto3.resource('s3',aws_access_key_id=cc.get("access_key_id"), aws_secret_access_key=cc.get("secret_access_key"))
+
+    bucket = s3.Bucket(bucket)
+    versions = bucket.object_versions.filter(Prefix = filename)
+
+    for version in versions:
+        try:
+            object = version.get()
+            path = version.object_key
+            last_modified = object.get('LastModified')
+            version_id = object.get('VersionId')
+            print(path, last_modified, version_id, sep = '\t')
+
+            object = version.get()
+            filename = path.rsplit('/')[-1]
+            with open('vers/{0}-{1}-{2}'.format(str(last_modified)[:10],version_id,filename), 'wb') as fout:
+                shutil.copyfileobj(object.get('Body'), fout)
+        except:
+            pass
+
+def delete_all_files(file_path):
+    for filename in os.listdir(file_path):
+        try:
+            print("deleted " + os.path.join(file_path, filename))
+            os.remove(os.path.join(file_path, filename))
+        except:
+            print("failed deleting " + os.path.join(file_path, filename))
+            pass
+
+import importlib as ip, sys
+results = []
+# def execution(file, fp = root_fp):
+def execution(file, fp ):
+    try:
+        sys.path.insert(0,root_fp)
+        global start_time
+        start_time = now(2)
+        im = ip.import_module(file)
+        end_time = now(2)
+        if len(im.results) > 1:
+            for items in im.results:
+                results.append(str(items) )
+                print(str(items))
+        else:
+            results.append(str(im.results[0])+' Started at '+ start_time +' ended at ' + end_time )
+            print(str(im.results[0])+' Started at '+ start_time +' ended at ' + end_time )
+    except:
+        end_time = now(2)
+        results.append(file.capitalize()+' job failed! Started at '+ start_time +' ended at ' + end_time)
+        print(file.capitalize()+' job failed! Started at '+ start_time +' ended at ' + end_time)
+    # return results
